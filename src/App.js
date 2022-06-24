84d036ea8cc0943d8d1a1123d8c464437671b949
Mariana Zambarda <mariana.z@citifyd.com>
2022-06-24 17:51:51 -0300

    added translation with i18next

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss'
 
 //components
-import Menu from './components/Menu/index.js';
+import Menu from './components/Menu/index.js'
 
 //pages
-import HomePage from './pages/HomePage/index.js';
-import AboutPage from './pages/AboutPage/index.js';
-import ProjectsPage from './pages/ProjectsPage/index.js';
-import ContactPage from './pages/ContactPage/index.js';
+import HomePage from './pages/HomePage/index.js'
+import AboutPage from './pages/AboutPage/index.js'
+import ProjectsPage from './pages/ProjectsPage/index.js'
+import ContactPage from './pages/ContactPage/index.js'
 
-function App() {
-	return (
-		<div className="section-content">
-			<Menu />
-			<HomePage />
-			<AboutPage />
-			<ProjectsPage />
-			<ContactPage />
-		</div>
-	);
+function App () {
+  return (
+    <div className='section-content'>
+      <Menu />
+      <HomePage />
+      <AboutPage />
+      <ProjectsPage />
+      <ContactPage />
+    </div>
+  )
 }
 
-export default App;
+export default App
