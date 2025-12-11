7aeec26a55899d49f5486e0a2096e78cacba608a
Mariana Zambarda <marianazambarda@gmail.com>
2025-12-11 15:11:42 -0300

    convert cra to vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import reportWebVitals from "./reportWebVitals";
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 
@@ -28,16 +27,18 @@ i18next.on("languageChanged", (language) => {
   localStorage.setItem("language", language);
 });
 
-ReactDOM.render(
+const rootElement = document.getElementById("root") as HTMLElement;
+
+if (!rootElement) {
+  throw new Error('Root element with id "root" not found');
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
       <App />
     </I18nextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
